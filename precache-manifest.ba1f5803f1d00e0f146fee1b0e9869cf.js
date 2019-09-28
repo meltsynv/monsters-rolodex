3660881fd81078132b8334700b07b58ca0b78131
Viktor Meltsyn <meltsynv@yahoo.com>
2019-09-29 01:37:22 +0200

    Updates

---
+++ precache-manifest.ba1f5803f1d00e0f146fee1b0e9869cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae2f47a2a12aae89a0424cdf2d2416e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "990245fc5817dac50e00",
+    "url": "/monsters-rolodex/static/css/main.c87b2398.chunk.css"
+  },
+  {
+    "revision": "4ee53b84b1f31b69fcd8",
+    "url": "/monsters-rolodex/static/js/2.01c3c59a.chunk.js"
+  },
+  {
+    "revision": "990245fc5817dac50e00",
+    "url": "/monsters-rolodex/static/js/main.72726854.chunk.js"
+  },
+  {
+    "revision": "61e64e548c9076aea34b",
+    "url": "/monsters-rolodex/static/js/runtime-main.c620570a.js"
+  }
+]);
